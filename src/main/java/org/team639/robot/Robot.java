268ef5f6290f30d6825ce77b72581f5417b31381
Jack Greenberg <theProgrammerJack@gmail.com>
2019-01-05 15:30:19 -0500

    Initial commit.

---
+++ src/main/java/org/team639/robot/Robot.java
@@ -0,0 +1,55 @@
+package org.team639.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+    @Override
+    public void robotInit() {
+        super.robotInit();
+    }
+
+    @Override
+    public void disabledInit() {
+        super.disabledInit();
+    }
+
+    @Override
+    public void autonomousInit() {
+        super.autonomousInit();
+    }
+
+    @Override
+    public void teleopInit() {
+        super.teleopInit();
+    }
+
+    @Override
+    public void testInit() {
+        super.testInit();
+    }
+
+    @Override
+    public void robotPeriodic() {
+        super.robotPeriodic();
+    }
+
+    @Override
+    public void disabledPeriodic() {
+        super.disabledPeriodic();
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        super.autonomousPeriodic();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        super.teleopPeriodic();
+    }
+
+    @Override
+    public void testPeriodic() {
+        super.testPeriodic();
+    }
+}
