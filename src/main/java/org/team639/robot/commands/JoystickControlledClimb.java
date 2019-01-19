442609e6c5c820fbc5c0838ba4c5a6375a4e6972
lillianHG <lillian.hwanggedde@icsd.k12.ny.us>
2019-01-19 14:53:29 -0500

    Created class for controlling the climbing system with a joystick (I'm not actually sure if this is really the best method, it might be more accurate to use a button and automate it)

---
+++ src/main/java/org/team639/robot/commands/JoystickControlledClimb.java
@@ -0,0 +1,10 @@
+package org.team639.robot.commands;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class JoystickControlledClimb extends Command{
+
+    public boolean isFinished()
+    {
+        return false;
+    }
+}
