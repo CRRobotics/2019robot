5a1bd06caed196204c2dcf04a75e9b009be4a1bc
Lillian Hwang-Geddes <lillivhg@gmail.com>
2019-01-22 17:45:23 -0500

    Added functionality for the joystick climbing

--- src/main/java/org/team639/robot/commands/JoystickControlledClimb.java
+++ src/main/java/org/team639/robot/commands/JoystickControlledClimb.java
@@ -1,7 +1,25 @@
 package org.team639.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.Joystick;
+import org.team639.robot.subsystems.Climbing;
 
 public class JoystickControlledClimb extends Command{
+
+    Joystick joystick;
+    Climbing climbing;
+
+    //Ports are placeholders and will need to be replaced
+    public JoystickControlledClimb()
+    {
+        joystick = new Joystick(0);
+        climbing = new Climbing();
+    }
+
+    public void execute()
+    {
+        double input = joystick.getRawAxis(0);
+        climbing.moveSystem(input);
+    }
 
     public boolean isFinished()
     {
