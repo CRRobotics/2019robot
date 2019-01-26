8b4d5ab10655b1b1fc908a5e8746376b8f6988c0
rohitlal274 <rohit.lal@icsd.k12.ny.us>
2019-01-26 16:35:57 -0500

    This is the start of lift code.

---
+++ src/main/java/org/team639/robot/Constants.java
@@ -0,0 +1,6 @@
+package org.team639.robot;
+
+public class Constants {
+    public static final Integer LIFT_MAX_HEIGHT = new Integer(8);
+
+}
---
+++ src/main/java/org/team639/robot/subsystems/Lift.java
@@ -0,0 +1,52 @@
+package org.team639.robot.subsystems;
+
+
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.team639.robot.Constants;
+
+
+public abstract class Lift extends Subsystem {
+
+    private final int lift_max_height;
+    private TalonSRX mainTalon;
+        private TalonSRX followerTalon;
+
+        private Solenoid brake;
+
+        private ControlMode currentControlMode;
+
+        private double kP;
+        private double kI;
+        private double kD;
+        private double kF;
+
+        public Lift() {
+            lift_max_height = 185;
+
+            mainTalon = new TalonSRX(6);
+            followerTalon = new TalonSRX(7);
+
+            followerTalon.follow(mainTalon);
+
+            mainTalon.setInverted(true);
+            followerTalon.setInverted(true);
+
+            mainTalon.configReverseSoftLimitEnable(false, 0);
+
+            mainTalon.configForwardSoftLimitEnable(true, 0);
+            mainTalon.configForwardSoftLimitThreshold(Constants.LIFT_MAX_HEIGHT, 0);
+
+
+    }
+
+}
