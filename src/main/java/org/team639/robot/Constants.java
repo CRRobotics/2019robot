4f5d288a5c34d0f303931831d91b2ae10ad60a9c
Aidan Foley <aidan.t.foley@gmail.com>
2019-01-28 17:58:45 -0500

    Added constants, although they are not yet assigned

--- src/main/java/org/team639/robot/Constants.java
+++ src/main/java/org/team639/robot/Constants.java
@@ -1,6 +1,26 @@
 package org.team639.robot;
 
 public class Constants {
-    public static final Integer LIFT_MAX_HEIGHT = new Integer(8);
+    public static final double LIFT_MAX_HEIGHT = 8.0f;
 
+    public enum liftPosition
+    {
+
+        // Height in feet from the bottom of the lift
+        // Heights to be implemented
+        TOP_BALL(0.0),
+        TOP_HATCH(0.0),
+        MIDDLE_BALL(0.0),
+        MIDDLE_HATCH(0.0),
+        BOTTOM_BALL(0.0),
+        BOTTOM_HATCH(0.0),
+        CARGO_SHIP(0.0);
+
+        double position;
+
+        liftPosition(double position)
+        {
+            this.position = position;
+        }
+    }
 }
